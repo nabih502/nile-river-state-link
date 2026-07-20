f19ac212ac8ef9b09c8502371fff6b456833c55c
nabih502 <eng.nabih.502@gmail.com>
2026-07-20 19:39:49 +0300

    Fix Bolt project saving issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: { useFsEvents: false, usePolling: true },
-  },
-  preview: {
-    port: 3000,
-    host: true,
-  },
 });
